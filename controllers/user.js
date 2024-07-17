a5f0620f452be749ce7cb201db75dc665485e056
fjzamora93 <fjzamora93@gmail.com>
2024-07-17 11:48:28 +0200

    pagination

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,42 @@
 const RecetaMdb = require('../models/recipeMdb'); 
 
-exports.getIndex = (req, res, next) =>{
-    const user = req.user;
+const ITEMS_PER_PAGE = 2;
+
+
+exports.getIndex = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
+  
     RecetaMdb.find()
-        .then(recetas => {
-            res.render('index', {
-                recetas:recetas,
-                user: user
-            })
-        })
-        .catch(err => console.log(err))
-}
+      .countDocuments()
+      .then(numProducts => {
+        totalItems = numProducts;
+        return RecetaMdb.find()
+          .skip((page - 1) * ITEMS_PER_PAGE)
+          .limit(ITEMS_PER_PAGE);
+      })
+      .then(products => {
+        res.render('index', {
+          recetas: products,
+          pageTitle: 'Shop',
+          path: '/',
+          currentPage: page,
+          hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+          hasPreviousPage: page > 1,
+          nextPage: page + 1,
+          previousPage: page - 1,
+          lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+        });
+      })
+      .catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+      });
+  };
+
+
+
 
 
 exports.getRecipeDetails = (req, res, next) => {
