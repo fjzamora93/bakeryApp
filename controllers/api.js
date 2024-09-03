66d769af8a4f0da656b5b030002bde2fb5d5636e
fjzamora93 <fjzamora93@gmail.com>
2024-09-03 17:13:10 -0400

    update bookmark system

--- controllers/api.js
+++ controllers/api.js
@@ -257,7 +257,9 @@ exports.putBookmark = async (req, res, next) => {
         }
 
         if (user.bookmark.includes(postId)) {
-            return res.status(409).json({ error: 'Post already bookmarked' });
+            user.bookmark.pull(postId);
+            await user.save();
+            return res.status(200).json({ message: 'Bookmark eliminado correctamente!' });
         }
 
         user.bookmark.push(postId);
